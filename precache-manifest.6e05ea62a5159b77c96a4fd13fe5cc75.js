0aba0464a74bda9aa0d94e45851078998d8c9d19
harris2310 <harris2310@hotmail.com>
2020-02-16 12:36:46 +0200

    Updates

---
+++ precache-manifest.6e05ea62a5159b77c96a4fd13fe5cc75.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dd3277ec6a658d81cf73dbf076f13a5",
+    "url": "hanozidis.site/index.html"
+  },
+  {
+    "revision": "16da31e66af5a4c9d080",
+    "url": "hanozidis.site/static/css/2.4121280a.chunk.css"
+  },
+  {
+    "revision": "3cceee0204d0d6700c1d",
+    "url": "hanozidis.site/static/css/main.3438266d.chunk.css"
+  },
+  {
+    "revision": "16da31e66af5a4c9d080",
+    "url": "hanozidis.site/static/js/2.c5ab5270.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "hanozidis.site/static/js/2.c5ab5270.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cceee0204d0d6700c1d",
+    "url": "hanozidis.site/static/js/main.ea459dfc.chunk.js"
+  },
+  {
+    "revision": "34fd372de9a0e9fd42da",
+    "url": "hanozidis.site/static/js/runtime-main.6f96479a.js"
+  },
+  {
+    "revision": "08b9fb296dcd928e42a90b21cb7d59b3",
+    "url": "hanozidis.site/static/media/Me.08b9fb29.jpg"
+  },
+  {
+    "revision": "9ba7ab8bdfe3f47b72218acc21feb411",
+    "url": "hanozidis.site/static/media/stones.9ba7ab8b.jpg"
+  }
+]);
